59661ce82d175ba083a62abdc16f964191ac434c
Ida Lam <idaslam@outlook.com>
2021-09-07 16:38:45 +0200

    added react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import Home from './views/home'
+
+const queryClient = new QueryClient()
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </QueryClientProvider>
     )
 }
 
