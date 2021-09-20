71d2380a290b2a7f77ca8fba8981df28bfc42a6b
Ida Lam <idaslam@outlook.com>
2021-09-20 14:25:53 +0200

    useSocket hook, which sets data

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import socket from '../sockets'
+
+type Doc = {
+    _id: string
+    title: string
+    content: string
+}
+
+const useSocket = (name: string) => {
+    const [data, setData] = useState<Doc | null>(null)
+
+    useEffect(() => {
+        socket.on(name, (doc: Doc) => {
+            setData(doc)
+        })
+
+        return () => {
+            socket.off(name)
+        }
+    }, [])
+
+    return data
+}
+
+export default useSocket
