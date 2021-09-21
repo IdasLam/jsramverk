220211f9ce6812cf7626ef33e0fcdda108c4e49f
Ida Lam <idaslam@outlook.com>
2021-09-21 18:57:53 +0200

    added rotue for login and create user

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 import Home from './views/home'
+import Login from './views/login'
 
 const queryClient = new QueryClient()
-
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <Router>
                 <Switch>
-                    <Route path="/">
-                        <Home />
+                    <Route exact path="/">
+                        <Login />
                     </Route>
+                    {localStorage.getItem('token') && (
+                        <Route exact path="/doc">
+                            <Home />
+                        </Route>
+                    )}
                 </Switch>
             </Router>
         </QueryClientProvider>
