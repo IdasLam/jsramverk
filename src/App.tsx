e22802d352bf8317bfbe35d7faef91c62c2382f8
Ida Lam <idaslam@outlook.com>
2021-09-22 19:13:46 +0200

    added jwt package to get expiry

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { useJwt } from 'react-jwt'
 
 import Home from './views/home'
 import Login from './views/login'
 
 const queryClient = new QueryClient()
 function App() {
+    const { isExpired } = useJwt(localStorage.getItem('token') ?? '')
+
     return (
         <QueryClientProvider client={queryClient}>
             <Router>
+                {isExpired && <Redirect to="/" />}
                 <Switch>
                     <Route exact path="/">
                         <Login />
                     </Route>
-                    {localStorage.getItem('token') && (
-                        <Route exact path="/doc">
-                            <Home />
-                        </Route>
-                    )}
+                    <Route exact path="/doc">
+                        <Home />
+                    </Route>
                 </Switch>
             </Router>
         </QueryClientProvider>
