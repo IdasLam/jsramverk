587ee5c5e54359c9943bf8549556d528156a4e3c
Ida Lam <idaslam@outlook.com>
2021-09-29 22:49:46 +0200

    added ability to send cookies with socket

--- src/sockets.ts
+++ src/sockets.ts
@@ -4,6 +4,6 @@ const ENDPOINT = window.location.hostname.includes('localhost')
     ? 'http://127.0.0.1:1337'
     : 'https://jsramverk-editor-idla18.azurewebsites.net'
 
-const socket = socketIOClient(ENDPOINT)
+const socket = socketIOClient(ENDPOINT, { withCredentials: true, autoConnect: false })
 
 export default socket
