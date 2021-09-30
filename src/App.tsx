ec2b19962234307cb75b7285c36f014cce055dc5
Ida Lam <idaslam@outlook.com>
2021-09-30 18:33:41 +0200

    root changes when on stud server and on localhost

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import Home from './views/home'
 import Login from './views/login'
 import { isLoggedIn } from './helpers/login'
+import { root } from './helpers/root'
 
 function App() {
     const loggedIn = isLoggedIn()
@@ -12,12 +13,12 @@ function App() {
 
     return (
         <Router>
-            {loggedin && <Redirect to="/" />}
+            {loggedin && <Redirect to={root} />}
             <Switch>
-                <Route exact path="/">
+                <Route exact path={root}>
                     <Login />
                 </Route>
-                <Route exact path="/doc">
+                <Route exact path={`${root}doc`}>
                     <Home />
                 </Route>
             </Switch>
