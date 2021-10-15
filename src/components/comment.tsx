9db1b2ccc89739a96ee309b538c28a2643d0d0e8
Ida Lam <idaslam@outlook.com>
2021-10-15 16:22:59 +0200

    comment component

---
+++ src/components/comment.tsx
@@ -0,0 +1,71 @@
+import { TextField, Button } from '@mui/material'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import socket from '../sockets'
+
+type Comment = {
+    x: number
+    y: number
+    onClose: () => void
+    data: CommentData | null
+    id: string | null
+    text?: string
+}
+
+type CommentData = {
+    selected: string
+    y: number
+    start: number
+    end: number
+}
+
+const CommentOption: React.FC<Comment> = ({ x, y, text, onClose, data, id }) => {
+    const [comment, setCommet] = useState<string>()
+
+    return (
+        <>
+            <Bg onClick={() => onClose()} />
+            <Container x={x} y={y}>
+                <TextField id="outlined" label="Comment" onChange={(event) => setCommet(event.target.value)} />
+                <Button
+                    variant="contained"
+                    onClick={() => {
+                        // console.log({text: })
+                        console.log({ id, text, data: { ...data, comment: comment } })
+                        // socket.emit('comment', { id, data: { ...data, comment: comment } })
+                        // also close comment
+                    }}
+                >
+                    Comment
+                </Button>
+            </Container>
+        </>
+    )
+}
+
+const Bg = styled.div`
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    top: 0px;
+    left: 0px;
+`
+
+const Container = styled.div<{ x: number; y: number }>`
+    position: fixed;
+    left: ${(props) => props.x}px;
+    top: ${(props) => props.y}px;
+    background-color: white;
+    width: fit-content;
+    box-shadow: 0px 0px 15px -6px rgba(0, 0, 0, 0.76);
+    cursor: pointer;
+    padding: 10px;
+    display: grid;
+
+    button {
+        width: 100%;
+        margin: 10px 0px 0px;
+    }
+`
+
+export default CommentOption
